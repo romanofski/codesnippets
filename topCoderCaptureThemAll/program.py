2c4faa60526bb0be09768941dee2d5353dc49711
Roman Joost <roman@bromeco.de>
2011-01-03 11:59:54 +1000

    BFS implementation

--- topCoderCaptureThemAll/program.py
+++ topCoderCaptureThemAll/program.py
@@ -2,14 +2,63 @@
 
 class CaptureThemAll(object):
 
-    board = (8,8)
+    board = (9,9)
+    searchmax = 10
+    queen_found = False
+    rook_found = False
 
     def __init__(self, knight, queen, rook):
-        self.knight = knight
-        self.queen = queen
-        self.rook = rook
+        self.knight = self.get_coordinates(knight)
+        self.queen = self.get_coordinates(queen)
+        self.rook = self.get_coordinates(rook)
+        self.visited = []
 
     def get_coordinates(self, strpos):
         pos = tuple(strpos)
-        rows = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+        rows = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
         return (rows.index(pos[0]) + 1, int(pos[1]))
+
+    def search(self, pos=None, moves=None, length=1):
+        if pos is None:
+            pos = self.knight
+        if moves is None:
+            moves = self.get_possible_knight_moves(pos)
+
+        for m in moves:
+            if m == self.queen:
+                self.queen_found = True
+            if m == self.rook:
+                self.rook_found = True
+
+        while moves:
+            if (True, True) == (self.queen_found, self.rook_found):
+                break
+
+            newpos = moves[0]
+            del moves[0]
+            moves = self.get_possible_knight_moves(newpos)
+
+            length += 1
+            self.search(newpos, moves, length)
+        return length
+
+    def get_possible_knight_moves(self, pos):
+        self.visited.append(pos)
+        moves = []
+        for x, y in [(2, 1), (2, -1), (-2, 1), (-2, -1)]:
+            newpos = (pos[0] + x, pos[1] + y)
+            if newpos in self.visited:
+                continue
+
+            if not self.is_out_of_bounds(newpos):
+                moves.append(newpos)
+            newpos = list(newpos)
+            newpos.reverse()
+            if not self.is_out_of_bounds(newpos):
+                moves.append(tuple(newpos))
+
+        return moves
+
+    def is_out_of_bounds(self, pos):
+        return min(pos) <= 1 or max(pos) >= self.board[0]
+
--- topCoderCaptureThemAll/test_program.py
+++ topCoderCaptureThemAll/test_program.py
@@ -11,3 +11,24 @@ class CaptureThemAllTest(unittest.TestCase):
 
         self.assertEquals(cta.get_coordinates('b3'), (2, 3))
         self.assertEquals(cta.get_coordinates('c5'), (3, 5))
+        self.assertEquals(cta.get_coordinates('h8'), (8, 8))
+        self.assertEquals(cta.knight, (1, 1))
+
+    def test_get_possible_knight_moves(self):
+        cta = program.CaptureThemAll('a1', 'b3', 'c5')
+        result = cta.get_possible_knight_moves((1, 1))
+        self.assertEquals(len(result), 2)
+        self.assertEquals(sorted(result), [(2, 3), (3, 2)])
+
+        result = cta.get_possible_knight_moves((4, 4))
+        self.assertEquals(len(result), 8)
+
+    def test_search(self):
+        cta = program.CaptureThemAll('a1', 'b3', 'c5')
+        result = cta.search()
+        self.assertEquals(result, 2)
+
+        cta = program.CaptureThemAll('b1', 'c3', 'a3')
+        result = cta.search()
+        self.assertEquals(result, 3)
+
