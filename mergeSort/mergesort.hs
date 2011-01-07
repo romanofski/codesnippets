94e89354227ac90a396c6e7bfe838bb006e09fa0
Roman Joost <roman@bromeco.de>
2011-01-07 18:55:23 +1000

    Finished implementing a merge sort

--- mergeSort/mergesort.hs
+++ mergeSort/mergesort.hs
@@ -1,10 +1,27 @@
-msort :: [a] -> [a]
+-- merge sort
+-- 1. if the list has only one element, it's sorted
+-- 2. split the list into two lists in the middle and reapply merge sort
+-- 3. sort each two lists by comparing the elements into a merged list
+mergesort :: (Ord a) => [a] -> [a]
+mergesort []     = error "Can't sort an empty list"
+mergesort (x:[]) = [x]
+mergesort x      = mergelist (mergesort leftlist, mergesort rightlist)
+    where h = halflist x
+          leftlist = fst h
+          rightlist = snd h
 
-msort [] = []
-msort (x:[]) = [x]
+
+mergelist :: (Ord a) => ([a], [a]) -> [a]
+mergelist ([], []) = []
+mergelist (xs, []) = xs
+mergelist ([], ys) = ys
+mergelist (left@(x:xs), right@(y:ys))
+    | x < y = [x] ++ mergelist (xs, right)
+    | otherwise = [y] ++ mergelist (left, ys)
+
 
 halflist :: [a] -> ([a], [a])
-halflist x = (leftlist, rightlist)
-    where middle = (length x) `div` 2
-          leftlist = take middle x
-          rightlist = drop middle x
+halflist l = (left, right)
+    where middle = (length l) `div` 2
+          left   = take middle l
+          right  = drop middle l
