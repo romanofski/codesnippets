b66a67972d5df24759dfcaf57157eb741c99f7eb
Roman Joost <roman@bromeco.de>
2011-01-09 21:06:50 +1000

    Completed binary search with helper function

--- binSearch/binsearch.hs
+++ binSearch/binsearch.hs
@@ -1,32 +1,18 @@
 binsearch :: Ord a => a -> [a] -> Int
 binsearch x []   = error "Item not in list or empty list"
 binsearch x [y]  = 1
-binsearch x xs
-    | x > m = binsearch x (slicelist i l xs)
-    | x < m = 2
-    | otherwise = i
-        where (m, i) = getmiddle xs
+binsearch x xs = search x 0 (length xs) xs
 
-
-slicelist :: Int -> Int -> [a] -> [a]
-slicelist a b [] = []
-slicelist a b [x] = [x]
-slicelist a b xs = drop a (take b xs)
-
--- get
--- returns an element e by given index i from a list
-get :: Int -> [a] -> a
-get i [] = error "empty list"
-get i [x] = x
-get i xs = head (slicelist n i xs)
-    where n = i - 1
-
--- getmiddle
+-- search
 -- returns a tuple with (element, index)
 -- we always return a middle, even if the length is uneven
-getmiddle :: [a] -> (a, Int)
-getmiddle []  = error "empty list"
-getmiddle [x] = (x, 0)
-getmiddle xs  = (x, i)
-    where i = (length xs) `div` 2
-          x = head (drop i xs)
+search :: Ord a => a -> Int -> Int -> [a] -> Int
+search e i j []  = error "empty list"
+search e i j [x] = 0
+search e i j xs
+    | e > li = search e m l xs
+    | e < li = search e 0 m xs
+    | otherwise = i
+        where m = (i + ((j - i) `div` 2))
+              l = length xs
+              li = head (drop m xs)
