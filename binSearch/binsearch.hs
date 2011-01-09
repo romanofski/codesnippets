3e046ae59b75cf14c9007a80873bf3c5b6b450f0
Roman Joost <roman@bromeco.de>
2011-01-09 21:20:11 +1000

    Added condition to catch given items which are not in the given list.
    Found small bug, which returned the middle +1 -1 a.k.a. not the correct
    calculated middle.

--- binSearch/binsearch.hs
+++ binSearch/binsearch.hs
@@ -1,18 +1,19 @@
 binsearch :: Ord a => a -> [a] -> Int
-binsearch x []   = error "Item not in list or empty list"
-binsearch x [y]  = 1
-binsearch x xs = search x 0 (length xs) xs
+binsearch x []   = error "Empty list"
+binsearch x [y]  = 0
+binsearch x xs = case (x `elem` xs) of True -> search x 0 (length xs) xs
+                                       False -> error "Element not in list"
 
 -- search
--- returns a tuple with (element, index)
--- we always return a middle, even if the length is uneven
+-- It takes the element e, lower i and upper boundary j of the list xs
+-- and returns the index of the given element e.
 search :: Ord a => a -> Int -> Int -> [a] -> Int
 search e i j []  = error "empty list"
 search e i j [x] = 0
 search e i j xs
     | e > li = search e m l xs
     | e < li = search e 0 m xs
-    | otherwise = i
+    | otherwise = m
         where m = (i + ((j - i) `div` 2))
               l = length xs
               li = head (drop m xs)
