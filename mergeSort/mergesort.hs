8244128d59e20f1a7c8db99caaea06bc60f86125
Roman Joost <roman@bromeco.de>
2011-01-17 18:57:20 +1000

    Added unittests using HUnit

--- mergeSort/mergesort.hs
+++ mergeSort/mergesort.hs
@@ -1,3 +1,5 @@
+import qualified Test.HUnit
+
 -- merge sort
 -- 1. if the list has only one element, it's sorted
 -- 2. split the list into two lists in the middle and reapply merge sort
@@ -25,3 +27,11 @@ halflist l = (left, right)
     where middle = (length l) `div` 2
           left   = take middle l
           right  = drop middle l
+
+-- tests
+test_halflist = Test.HUnit.TestCase (Test.HUnit.assertEqual "halflist" (halflist [1,3,4]) ([1], [3,4]))
+test_mergesort = Test.HUnit.TestCase (Test.HUnit.assertEqual "mergesort" (mergesort [42,38,5,12,22]) [5,12,22,38,42])
+tests = Test.HUnit.TestList [Test.HUnit.TestLabel "test halflist" test_halflist,
+                             Test.HUnit.TestLabel "test mergesort" test_mergesort]
+
+testrunner = Test.HUnit.runTestTT tests
