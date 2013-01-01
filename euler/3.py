48b70e752b4d81f8f679b75ecbbb534f8ae1c11a
Roman Joost <roman@bromeco.de>
2013-01-01 20:11:55 +1000

    Reimplementation of the prime generator and solution.

--- euler/3.py
+++ euler/3.py
@@ -1,6 +1,25 @@
+import timeit
 
 
 def get_primes(limit=50):
+    """ prime number generator, sieve of Eratosthenes
+
+    >>> get_primes(5)
+    [2, 3, 5]
+    >>> get_primes(8)
+    [2, 3, 5, 7]
+    >>> get_primes(20)
+    [2, 3, 5, 7, 11, 13, 17, 19]
+    """
+    primes = [2, 3]
+    for x in xrange(4, limit + 1):
+        if x % 2 == 0 or x % 3 == 0:
+            continue
+        primes.append(x)
+    return primes
+
+
+def get_primes_old(limit=50):
     """ prime number generator, sieve of Eratosthenes
 
     >>> get_primes(5)
@@ -32,6 +51,8 @@ def prime_factors(number, solution=None):
     [2, 2, 3]
     >>> prime_factors(13195)
     [5, 7, 13, 29]
+    >>> prime_factors(330)
+    [2, 3, 5, 11]
     """
     if solution is None:
         solution = []
@@ -49,4 +70,8 @@ def prime_factors(number, solution=None):
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
-    print prime_factors(600851475143)
+    # print timeit.Timer("get_primes_old()", "from __main__ import get_primes_old").timeit()
+    # print timeit.Timer("get_primes()", "from __main__ import get_primes").timeit()
+    fact = prime_factors(600851475143)
+    print fact
+    print reduce(lambda x, y: x * y, fact) == 600851475143
