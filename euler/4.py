f011f9b324ff62da97cb5c25534560a82e08f9a1
Roman Joost <roman@bromeco.de>
2013-01-03 17:33:44 +1000

    First implementation was faulty

--- euler/4.py
+++ euler/4.py
@@ -7,15 +7,20 @@ def is_palindrome(mapping):
     True
     >>> is_palindrome('ottof')
     False
+    >>> is_palindrome(91219)
+    True
+    >>> is_palindrome(9219)
+    False
     """
-    mapping = list(mapping)
-    for x in mapping:
+    mapping = list(str(mapping))
+    index = 0
+    while mapping and index < len(mapping):
+        x = mapping[index]
         y = mapping.pop()
-        if x == y:
-            return True
-        else:
-            break
-    return False
+        if x != y:
+            return False
+        index += 1
+    return True
 
 
 if __name__ == '__main__':
