c4d05224f8f63b1d700c830ad91cbf65217adcf7
Roman Joost <roman@bromeco.de>
2014-01-17 16:15:51 +1000

    11th problem :)

--- haskell99problems/seconddec.hs
+++ haskell99problems/seconddec.hs
@@ -7,12 +7,26 @@ import Data.List
 -- modified version of the run-length encoding
 -- no duplicates are simply copied into the list
 --
-data Encoded a =  Single a | Multiple Int a deriving Show
+data ListItem a =  Single a | Multiple Int a deriving Show
 
-encode :: Eq a => [a] -> [Encoded a]
-encode [x] = [Single x]
-encode xs = foldl (\a b -> if length b == 1 then (Single (head b)):a else (Multiple (length b) (head b)):a) [] (group xs)
--- TODO: wrong order
--- yuck!
+encodeModified :: Eq a => [a] -> [ListItem a]
+encodeModified xs = foldr (\b a -> if length b == 1
+                then (Single (head b)):a
+                else (Multiple (length b) (head b)):a)
+            []
+            (group xs)
+
+
+-- run-length encoding of a list (problem 10)
+encodeSimple :: Ord a => [a] -> [(Int, a)]
+encodeSimple xs = zip (map length $ group xs) (map head $ group xs)
+
+-- encode' - modified solution for problem 11
+-- uses map instead of foldr
+-- credit goes to the author of the 11th solution on the haskell wiki
+encodeModified' :: Ord a => [a] -> [ListItem a]
+encodeModified' = map encodeHelper . encodeSimple
+    where encodeHelper (1, x) = Single x
+          encodeHelper (n, x) = Multiple n x
 
 main = $(quickCheckAll)
