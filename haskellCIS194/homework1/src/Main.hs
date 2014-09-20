f16304d0f4afa7a5b0095106d154fbf2dc7b88c9
Roman Joost <roman@bromeco.de>
2014-09-20 16:08:26 +1000

    replaced `init` with proper arithmetic operators
    This patch is replacing the totally retarded implementation with a less
    retarded implementation. Yet it still feels retarded.

--- haskellCIS194/homework1/src/Main.hs
+++ haskellCIS194/homework1/src/Main.hs
@@ -8,10 +8,27 @@ main = print $ show $ dropLastDigit 12345
 -- basically a `last` on a given integer
 -- Problems:
 --  * character to (Int to ) Integer
+-- |
+-- >>> lastDigit 123
+-- 3
+-- >>> lastDigit 3
+-- 3
+-- >>> lastDigit 53234
+-- 4
 lastDigit :: Integer -> Integer
-lastDigit x = toInteger $ digitToInt $ last $ show x
+lastDigit x =  abs (r - x)
+    where r = (round (toRational (x) * 0.1)) * 10
 
+
+-- |
+-- >>> dropLastDigit 5
+-- 0
+-- >>> dropLastDigit 1234
+-- 123
+-- >>> dropLastDigit 1235
+-- 123
 dropLastDigit :: Integer -> Integer
 dropLastDigit x
     | x < 10     = 0
-    | otherwise  = read $ init $ show x :: Integer
+    | otherwise  = floor $ y
+    where y = toRational (x) * 0.1
