25c424cc0e53e38ca531f2d9230776baa2c9ea9e
Roman Joost <roman@bromeco.de>
2014-09-30 16:09:18 +1000

    Consolidate helper functions, which are not really needed

--- haskellCIS194/homework2/LogAnalysis.hs
+++ haskellCIS194/homework2/LogAnalysis.hs
@@ -3,31 +3,7 @@ module LogAnalysis where
 
 import Log
 
--- | Exercise 1
--- >>> toError ["I", "29", "la", "la"]
--- Info
--- >>> toError ["W", "29", "la"]
--- Warning
--- >>> toError ["E", "2", "562", "help", "help"]
--- Error 2
-toError :: [String] -> MessageType
-toError ("I":_) = Info
-toError ("W":_) = Warning
-toError ("E":x:_) = Error (read x :: Int)
-toError _ = error "Unkown Error code"
-
-
--- |
--- >>> toTimeStamp ["I", "29", "la"]
--- 29
--- >>> toTimeStamp ["E","2","30","foo"]
--- 30
-toTimeStamp :: [String] -> TimeStamp
-toTimeStamp xs = case xs of
-                ("E":_:x:_) -> read x :: TimeStamp
-                (_:x:_) -> read x :: TimeStamp
-
--- |
+-- | Helper function for Exercise 1
 -- >>> toLogMessage ["I", "29", "la"]
 -- ValidLM (LogMessage Info 29 "la")
 -- >>> toLogMessage ["E", "2", "29", "help"]
@@ -35,11 +11,12 @@ toTimeStamp xs = case xs of
 -- >>> toLogMessage ["Foo", "bar", "baz"]
 -- InvalidLM "Foo bar baz"
 toLogMessage :: [String] -> MaybeLogMessage
-toLogMessage p@("I":_) = ValidLM (LogMessage (toError p) (toTimeStamp p) (unwords $ drop 2 p))
-toLogMessage p@("E":_) = ValidLM (LogMessage (toError p) (toTimeStamp p) (unwords $ drop 3 p))
+toLogMessage ("I":y:xs) = ValidLM (LogMessage Info (read y :: TimeStamp) (unwords xs))
+toLogMessage ("W":y:xs) = ValidLM (LogMessage Warning (read y :: TimeStamp) (unwords xs))
+toLogMessage ("E":x:y:xs) = ValidLM (LogMessage (Error (read x)) (read y) (unwords xs))
 toLogMessage p = InvalidLM (unwords p)
 
--- |
+-- | Exercise 1
 -- >>> parseMessage "E 2 562 help help"
 -- ValidLM (LogMessage (Error 2) 562 "help help")
 -- >>> parseMessage "I 29 la la la"
