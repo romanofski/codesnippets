3a5b4e2f98b430b585044ea07bc9b56065faf61d
Roman Joost <roman@bromeco.de>
2014-09-30 16:48:51 +1000

    exercise 2 code

--- haskellCIS194/homework2/LogAnalysis.hs
+++ haskellCIS194/homework2/LogAnalysis.hs
@@ -25,3 +25,22 @@ toLogMessage p = InvalidLM (unwords p)
 -- InvalidLM "Not the right format"
 parseMessage :: String -> MaybeLogMessage
 parseMessage s = toLogMessage (words s)
+
+
+-- | Helper function
+-- >>> isValidLM (ValidLM (LogMessage (Error 2) 562 "hp"))
+-- True
+-- >>> isValidLM (parseMessage "not the right")
+-- False
+isValidLM :: MaybeLogMessage -> Bool
+isValidLM (ValidLM _) = True
+isValidLM _ = False
+
+-- | Exercise 2
+-- >>> validMessagesOnly [ValidLM (LogMessage (Error 2) 562 "hp"), InvalidLM "foo"]
+-- [LogMessage (Error 2) 562 "hp"]
+validMessagesOnly :: [MaybeLogMessage] -> [LogMessage]
+validMessagesOnly [] = []
+validMessagesOnly (ValidLM lm:xs) = lm:validMessagesOnly xs
+validMessagesOnly (_:xs) = validMessagesOnly xs
+
