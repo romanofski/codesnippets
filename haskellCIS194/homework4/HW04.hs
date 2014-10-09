b3c53dd29063aed170fa6c04be6e0c971c2a2d01
Roman Joost <roman@bromeco.de>
2014-10-09 16:00:16 +1000

    Exercise 13 - BST insertion

---
+++ haskellCIS194/homework4/HW04.hs
@@ -0,0 +1,18 @@
+module HW04 where
+
+import BST
+
+
+-- | Exercise 13: insert into a BST
+-- >>> insertBST compare 1 Leaf
+-- Node Leaf 1 Leaf
+-- >>> insertBST compare 2 (Node Leaf 1 Leaf)
+-- Node Leaf 1 (Node Leaf 2 Leaf)
+-- >>> insertBST compare (-1) (Node Leaf 1 (Node Leaf 2 Leaf))
+-- Node (Node Leaf (-1) Leaf) 1 (Node Leaf 2 Leaf)
+insertBST :: (a -> a -> Ordering) -> a -> BST a -> BST a
+insertBST _ n Leaf = Node Leaf n Leaf
+insertBST cmp n (Node left x right) = case cmp n x of
+    LT -> Node (insertBST cmp n left) x right
+    EQ -> Node (insertBST cmp n left) x right
+    GT -> Node left x (insertBST cmp n right)
