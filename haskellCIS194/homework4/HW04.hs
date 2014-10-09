e920d4550e99754cb92f98b1f39a728ae9550353
Roman Joost <roman@bromeco.de>
2014-10-09 16:32:27 +1000

    Avoid duplicates

--- haskellCIS194/homework4/HW04.hs
+++ haskellCIS194/homework4/HW04.hs
@@ -6,6 +6,8 @@ import BST
 -- | Exercise 13: insert into a BST
 -- >>> insertBST compare 1 Leaf
 -- Node Leaf 1 Leaf
+-- >>> insertBST compare 1 (Node Leaf 1 Leaf)
+-- Node Leaf 1 Leaf
 -- >>> insertBST compare 2 (Node Leaf 1 Leaf)
 -- Node Leaf 1 (Node Leaf 2 Leaf)
 -- >>> insertBST compare (-1) (Node Leaf 1 (Node Leaf 2 Leaf))
@@ -14,5 +16,5 @@ insertBST :: (a -> a -> Ordering) -> a -> BST a -> BST a
 insertBST _ n Leaf = Node Leaf n Leaf
 insertBST cmp n (Node left x right) = case cmp n x of
     LT -> Node (insertBST cmp n left) x right
-    EQ -> Node (insertBST cmp n left) x right
+    EQ -> Node left x right
     GT -> Node left x (insertBST cmp n right)
