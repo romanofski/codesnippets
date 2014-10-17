d0e1bf7a90276cc3301abe17ad3f5af9f917318e
Roman Joost <roman@bromeco.de>
2014-10-17 16:53:25 +1000

    New not yet working impl. of HW05

---
+++ haskellCIS194/homework5/HW05.hs
@@ -0,0 +1,25 @@
+module HW05 where
+
+import Ring
+import Parser
+
+
+-- | Exercise 1
+-- >>> intParsingWorks
+-- True
+intParsingWorks :: Bool
+intParsingWorks = (parse "3" == Just (3 :: Integer, "")) &&
+                  (parseRing "1 + 2 * 5" == Just (11 :: Integer)) &&
+                  (addId == (0 :: Integer))
+
+
+data Mod5 = MkMod Integer
+    deriving (Show, Eq)
+
+instance Ring Mod5 where
+    addId = MkMod 0
+    addInv (MkMod x) = MkMod (((-x `mod` 5) + (x `mod` 5)) `mod` 5)
+    mulId = MkMod 1
+
+    add (MkMod x) (MkMod y) = MkMod ((x + y) `mod` 5)
+    mul (MkMod x) (MkMod y) = MkMod ((x * y) `mod` 5)
