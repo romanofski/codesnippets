887a83adc4799097833a6e0498092d321b9aedfc
Roman Joost <roman@bromeco.de>
2014-11-14 14:08:33 +1000

    Improved Exercise 1
    I've actually realized, that I wasn't done with exercise one. The Value
    is not only a String, but can also be a hashmap or vector. Vector
    implementation is to come.

--- haskellCIS194/homework6/HW06.hs
+++ haskellCIS194/homework6/HW06.hs
@@ -15,16 +15,21 @@ import qualified Data.Text.IO               as T
 -- The code need the following language pragmas
 -- >>> :set -XOverloadedStrings
 -- >>> :set -XDeriveGeneric
+-- >>> import Data.HashMap.Strict
 
 -- | Exercise 1 - convert "Y" to Bool
 -- >>> ynToBool (String "Y")
 -- Bool True
+-- >>> ynToBool (Object (fromList [("country",String "Alameda"), ("credits",String "Y")]))
+-- Object (fromList [("country",String "Alameda"),("credits",Bool True)])
 -- >>> ynToBool (String "N")
 -- Bool False
 --
 ynToBool :: Value -> Value
 ynToBool (String "Y") = Bool True
-ynToBool _            = Bool False
+ynToBool (String "N") = Bool False
+ynToBool (String x) = String x
+ynToBool (Object o) = Object (fmap (ynToBool) o)
 
 
 -- | Exercise 2 - returns either an error or a Value
