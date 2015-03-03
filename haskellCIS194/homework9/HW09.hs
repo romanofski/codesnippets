0e94b73f4d47c4031bd7039f8074b35841d28122
Róman Joost <roman@bromeco.de>
2015-03-03 13:04:56 +1000

    Moved type annotation to apply props

--- haskellCIS194/homework9/HW09.hs
+++ haskellCIS194/homework9/HW09.hs
@@ -7,15 +7,22 @@ import Control.Monad (liftM4)
 
 main :: IO ()
 main = do
-  quickCheck prop_addIdM
-  quickCheck prop_addId
-  quickCheck prop_associative
-  quickCheck prop_addInv
-  quickCheck prop_commutative
-  quickCheck prop_isMonoid
-  quickCheck prop_mulId
-  quickCheck prop_leftDistributivity
-  quickCheck prop_rightDistributivity
+  quickCheck (prop_addId :: Mod5 -> Bool)
+  quickCheck (prop_addId :: Mat2x2 -> Bool)
+  quickCheck (prop_associative :: Mod5 -> Mod5 -> Mod5 -> Bool)
+  quickCheck (prop_associative :: Mat2x2 -> Mat2x2-> Mat2x2 -> Bool)
+  quickCheck (prop_addInv :: Mod5 -> Bool)
+  quickCheck (prop_addInv :: Mat2x2 -> Bool)
+  quickCheck (prop_commutative :: Mod5 -> Mod5 -> Bool)
+  quickCheck (prop_commutative :: Mat2x2 -> Mat2x2 -> Bool)
+  quickCheck (prop_isMonoid :: Mod5 -> Mod5 -> Mod5 -> Bool)
+  quickCheck (prop_isMonoid :: Mat2x2 -> Mat2x2 -> Mat2x2 -> Bool)
+  quickCheck (prop_mulId :: Mod5 -> Bool)
+  quickCheck (prop_mulId :: Mat2x2 -> Bool)
+  quickCheck (prop_leftDistributivity :: Mod5 -> Mod5 -> Mod5 -> Bool)
+  quickCheck (prop_leftDistributivity :: Mat2x2 -> Mat2x2 -> Mat2x2 -> Bool)
+  quickCheck (prop_rightDistributivity :: Mod5 -> Mod5 -> Mod5 -> Bool)
+  quickCheck (prop_rightDistributivity :: Mat2x2 -> Mat2x2 -> Mat2x2 -> Bool)
 
 -- | Exercise 1
 --
@@ -29,29 +36,18 @@ instance Arbitrary Mat2x2 where
 
 -- | Exercise 2
 --
-prop_associative :: Mod5 -> Mod5 -> Mod5 -> Bool
 prop_associative a b c = (a `add` b) `add` c == a `add` (b `add` c)
 
-prop_addId :: Mod5 -> Bool
 prop_addId x = x `add` addId == x
 
-prop_addInv :: Mod5 -> Bool
-prop_addInv x = addInv x == addId
+prop_addInv x = x `add` (addInv x) == addId
 
-prop_commutative :: Mod5 -> Mod5 -> Bool
 prop_commutative a b = a `add` b == b `add` a
 
-prop_isMonoid :: Mod5 -> Mod5 -> Mod5 -> Bool
 prop_isMonoid a b c = (a `mul` b) `mul` c == a `mul` (b `mul` c)
 
-prop_mulId :: Mod5 -> Bool
 prop_mulId a = a `mul` mulId == a
 
-prop_leftDistributivity :: Mod5 -> Mod5 -> Mod5 -> Bool
 prop_leftDistributivity a b c = a `mul` (b `add` c) == (a `mul` b) `add` (a `mul` c)
 
-prop_rightDistributivity :: Mod5 -> Mod5 -> Mod5 -> Bool
 prop_rightDistributivity a b c = (b `add` c) `mul` a == (b `mul` a) `add` (c `mul` a)
-
-prop_addIdM :: Mat2x2 -> Bool
-prop_addIdM x = x `add` addId == x
