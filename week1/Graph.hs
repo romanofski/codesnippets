042fc8e00cc6a4a9335f2e77ab58b05d77522ba3
Roman Joost <rjoost@redhat.com>
2015-03-23 16:18:33 +1000

    using a Vector for O(1) lookup
    Without the set, I haven't figured out how to append without avoiding
    duplicates. The addEdge suffers from wrong appends, but there is
    progress.

--- week1/Graph.hs
+++ week1/Graph.hs
@@ -1,19 +1,20 @@
+-- Graph using a List: no O(1) lookup
+--
 module Graph where
 
 -- import Test.QuickCheck
 -- import Control.Monad (replicateM)
-import Data.Set (Set, insert, empty, fromList)
+import Data.Vector
+import Prelude hiding (length)
 
 
 type Edge = Integer
 
-data Vertex = Vertex (Edge, Edge)
-    deriving Show
-
-data Node a = Node (Edge, Set Edge)
+data Node a = Empty
+            | Vertex Edge (Node a)
     deriving (Show, Eq)
 
-data Graph a = Graph [Node a]
+data Graph a = Graph (Vector (Node a))
     deriving Show
 
 
@@ -26,16 +27,23 @@ instance Arbitrary a => Arbitrary (Set a) where
 -}
 
 -- | Creates a graph from a list
--- >>> mkGraph (Graph []) [(1, 2), (2, 3), (2, 2)]
+-- >>> mkGraph [(1, 2), (2, 3), (2, 2)] (Graph [])
 -- Graph [Node (1,fromList [2]),Node (2,fromList [1,3])]
-mkGraph :: Graph a -> [(Edge, Edge)] -> Graph a
-mkGraph g ([]) = g
-mkGraph g ((x, y):xs) = mkGraph (addEdge g x y) xs
+mkGraph :: [(Integer, Integer)] -> Graph a -> Graph a
+mkGraph ([]) g = g
+mkGraph ((x, y):xs) g = mkGraph xs (addEdge (Vertex x (Vertex y Empty)) g)
 
--- | adds an Edge v w to the Graph
-addEdge :: Graph a -> Edge -> Edge -> Graph a
-addEdge (Graph []) a b                = Graph [Node (a, insert b empty), Node (b, insert a empty)]
-addEdge (Graph (Node (a, xs):_)) _ b  = Graph [Node (a, insert b xs)]
+-- | adds a Vertex to the Graph
+-- prop> addEdge x g == addEdge x (addEdge x g)
+addEdge :: Node a -> Graph a -> Graph a
+addEdge x@(Vertex _ (Vertex _ Empty)) (Graph xs) = Graph (x `cons` xs)
+addEdge x@(Vertex a b) (Graph xs) = case findEdge x xs of
+    Just (Vertex _ _) -> Graph xs
+    Nothing -> Graph (x `cons` xs)
+-- addEdge (Graph (Node (Vertex a, xs):_)) _ b  = Graph [Node (a, insert b xs)]
+
+findEdge :: Node a -> Vector (Node a) -> Maybe (Node a)
+findEdge x = find (== x)
 
 -- | returns the amount of vertices
 -- >>> let g = mkGraph (Graph []) [(1,2), (2, 3), (3, 3), (2, 5)]
@@ -44,10 +52,5 @@ addEdge (Graph (Node (a, xs):_)) _ b  = Graph [Node (a, insert b xs)]
 --
 -- Quickcheck property which doesn't work yet
 -- length [a] == numVertices [Node a]
-numVertices :: Graph a -> Integer
-numVertices (Graph []) = 0
-numVertices (Graph (Node _:xs)) = 1 + nodeLength xs
-    where
-        nodeLength :: [Node a] -> Integer
-        nodeLength [] = 0
-        nodeLength (_:rest) = 1 + nodeLength rest
+numVertices :: Graph a -> Int
+numVertices (Graph xs) = length xs
