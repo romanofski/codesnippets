8d0ab853a805f1f1dc0ac247222b31651cb24c7c
Róman Joost <roman@bromeco.de>
2018-11-27 14:01:41 +1000

    Support additional graph operations
    This adds support for additional graph operations. I first intended to use the
    `MapOperation` type to squeeze them in, but soon realized that transforming the
    graph is different from producing a result from the graph. Hence I decided to
    keep them separate.

--- courseraAlgorithms2/graphs/tools/ShortestPath.hs
+++ courseraAlgorithms2/graphs/tools/ShortestPath.hs
@@ -1,10 +1,14 @@
 import qualified Options.Applicative.Builder as Builder
 import Options.Applicative.Types (Parser, ReadM(..))
 import Options.Applicative (execParser, helper, (<**>))
+import Options.Applicative.Help.Pretty (text, Doc, vcat, (</>), linebreak)
+import Control.Applicative (many)
 
 import JSON
-       (fromFile, toGraph, parseOperations, MapOperation(..), encode,
-        shortestPathToType)
+       (fromFile, toGraph, parseOperations, MapOperation(..),
+        shortestPathToType, GraphOperation(..),
+        decodeString, encode)
+import Graph (Graph, replaceUndirectedEdge)
 import Dijkstra (getShortestPath)
 
 -- |
@@ -12,15 +16,37 @@ import Dijkstra (getShortestPath)
 mapOperation :: ReadM MapOperation
 mapOperation = Builder.eitherReader parseOperations
 
-data Config = Config FilePath MapOperation
+graphOperation :: ReadM GraphOperation
+graphOperation = Builder.eitherReader decodeString
+
+data Config = Config FilePath MapOperation [GraphOperation]
 
 configParser :: Parser Config
 configParser =
     Config <$>
-    Builder.argument Builder.str
-        (Builder.metavar "FILE" <>
-         Builder.help "File with map defined in JSON")
-    <*> Builder.argument mapOperation (Builder.metavar "OPS" <> Builder.help "Operations in JSON")
+    Builder.argument
+        Builder.str
+        (Builder.metavar "FILE" <> Builder.help "File with map defined in JSON") <*>
+    Builder.argument
+        mapOperation
+        (Builder.metavar "QUERY" <> Builder.help "Query the shortest path") <*>
+    many
+        (Builder.argument
+             graphOperation
+             (Builder.metavar "OPS" <>
+              Builder.help "Add/Replace points and routes"))
+
+verboseHelp :: Doc
+verboseHelp =
+    text "Example" </> linebreak </>
+    vcat
+        (text <$>
+         [ "Use the map.json for initialisation, calculate shortest path"
+         , "from A to J, add additional points I and J from point F and reduce the"
+         , "route weight from A to B to 20"]) </>
+    linebreak </>
+    text
+        "stack exec shortestpath tests/data/map.json '{\"start\":\"A\", \"end\":\"J\" }' '{ \"map\": [{ \"F\": {\"I\":70, \"J\":150} }]}' '{ \"A\": {\"B\":20} }'"
 
 main :: IO ()
 main = run =<< execParser opts
@@ -28,14 +54,20 @@ main = run =<< execParser opts
     opts = Builder.info (configParser <**> helper)
       (Builder.fullDesc
       <> Builder.progDesc "Finds shortest path"
+      <> Builder.progDescDoc (Just verboseHelp)
       <> Builder.header "shortestpath - a tool which finds the shortest path in a weighted undirected graph")
 
+applyGraphOperation :: Graph -> GraphOperation -> Graph
+applyGraphOperation g (Add g') = g' <> g
+applyGraphOperation g (ReplaceEdge e) = replaceUndirectedEdge e g
 
 run :: Config -> IO ()
-run (Config fp (QueryDistance s d)) = do
+run (Config fp query ops) = do
   json <- fromFile fp
   case json of
     Left err -> print err
-    Right m -> asJSON m
+    Right m -> do
+      let g = foldr (flip applyGraphOperation) (toGraph m) ops
+      print (go query g)
   where
-    asJSON = print . encode . shortestPathToType . getShortestPath s d . toGraph
+    go (QueryDistance s d) = encode . shortestPathToType . getShortestPath s d
